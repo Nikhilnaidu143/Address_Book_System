c35007d60af9d65329479c2c2144c7dd39645f68
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-12 07:04:13 +0530

    UC-1:- Address book with credentials(Contact-Info).

--- Contact_info.java
+++ Contact_info.java
@@ -1,5 +1,30 @@
 package com.Address_book_system;
 
 public class Contact_info {
-
+	String First_Name;
+	String Last_Name;
+	String Address;
+	String City;
+	String State;
+	int Zip_code;
+	int Phone_number;
+	String Email;
+	
+	public Contact_info(String fn, String ln, String add, String city, String state, int zip, int ph, String email) {
+		
+		this.First_Name = fn;
+		this.Last_Name = ln;
+		this.Address = add;
+		this.City = city;
+		this.State = state;
+		this.Zip_code = zip;
+		this.Phone_number = ph;
+		this.Email = email;
+	}
+	
+	public String toString() {
+		return String.format(First_Name + " " + Last_Name + " " + Address + " " + City + " " + State + " " + Zip_code + " " + Phone_number + " " + Email);
+	}
+	
+	
 }
