b79a9a20e41ee9f2b429e727768b010d4ff0b12b
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-11 19:28:20 +0530

    UC-6:- Add Multiple Address Book to the system feature is added.

---
+++ src/com/addressbooksystem/AddressBookList.java
@@ -0,0 +1,27 @@
+package com.addressbooksystem;
+
+import java.util.ArrayList;
+
+public class AddressBookList {
+	String userInputBookName;
+	ArrayList<Contact_info> contact;
+
+	// constructor
+	public AddressBookList(String userInputBookName) {
+		this.userInputBookName = userInputBookName;
+		this.contact = new ArrayList<Contact_info>();
+	}
+
+	public String getUserInputBookName() {
+		return userInputBookName;
+	}
+
+	public void setUserInputBookName(String userInputBookName) {
+		this.userInputBookName = userInputBookName;
+	}
+
+	@Override
+	public String toString() {
+		return "Address Book name :- " + userInputBookName + "\n" + "Contacts :- " + contact;
+	}
+}
